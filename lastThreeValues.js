ebb1c6492c6c55a004d0f09c191473bd84f9b05e
Chris Chu <cchu008@gmail.com>
2017-08-04 12:58:02 -0700

    function is able to return the last three values, given result_id input

--- lastThreeValues.js
+++ lastThreeValues.js
@@ -1,14 +1,47 @@
-var initialResults = require('./test.json');
+var initialResults = require('./initial_lab_results.json');
+var testResults = require('./test.json');
 
-var testFunction = function(results, inputID) {
+const findPatientIDandName = function(results, inputID) {
   let patientID, name;
-  initialResults.filter((result) => {
+  results.filter((result) => {
     if (result.result_id === inputID) {
       patientID = result.patient_id;
       name = result.name;
     }
   })
-  console.log(patientID, 'should match 1000180');
+  // console.log(patientID, 'should match', 1000000);
+  // console.log('name', name, 'should match HDL');
+
 }
 
-testFunction(initialResults, 217029);
+// findPatientIDandName(initialResults, 126);
+
+const resultsWithPatientIDandName = function(results, inputID) {
+  let patientID, name;
+
+  results.filter((result) => {
+    if (result.result_id === inputID) {
+      patientID = result.patient_id;
+      name = result.name;
+    }
+  })
+
+  console.log(patientID, 'should match', 1000000);
+  console.log('name', name, 'should match HDL');
+
+  var resultsOrderedByDate = results.filter((result) => {
+    return patientID === result.patient_id && name === result.name;
+  }).sort((a,b) => {
+    return new Date(a.date) - new Date(b.date);
+  })
+
+  var lastThreeValues = [
+    resultsOrderedByDate[resultsOrderedByDate.length - 1],
+    resultsOrderedByDate[resultsOrderedByDate.length - 2],
+    resultsOrderedByDate[resultsOrderedByDate.length - 3]
+  ]
+
+  return lastThreeValues;
+}
+
+console.log(resultsWithPatientIDandName(initialResults, 126));
